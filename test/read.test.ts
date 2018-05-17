5aa9be6548636c18cb2b62d7627ec2aecd02d974
Jovan Damjanovic <jovandamjanovic@gmail.com>
2018-05-17 16:41:35 +0200

    other tests

--- test/read.test.ts
+++ test/read.test.ts
@@ -5,7 +5,40 @@ import * as Hemera from 'nats-hemera';
 const expect = chai.expect;
 
 const testData: any = {
-    name: 'Vladimir',
+    fedwirePaymentId: '123456789',
+
+    originatorAddressLine1: 'originator address 1',
+    originatorAddressLine2: 'originator address 2',
+    originatorAddressLine3: 'originator address 3',
+
+    instructingFIIdCode: 'B - SWIFT Bank Identifier (BIC)',
+    instructingFIIdentifier: 'instructing FI Identifier',
+    instructingFIName: 'instructing FI Name',
+    instructingFIAddressLine1: 'instructing FI Address Line 1',
+    instructingFIAddressLine2: 'instructing FI Address Line 2',
+    instructingFIAddressLine3: 'instructing FI Address Line 3',
+
+    detailsOfCharges: 'details Of Charges',
+    detailsOfChargesCurrency: 'details Of Charges Currency',
+    sendersCharge1: 1000,
+    sendersCharge1Currency: 'senders Charge 1 Currency',
+    sendersCharge2: 1000,
+    sendersCharge2Currency: 'senders Charge 2 Currency',
+    instructedAmount: 1000,
+    instructedAmountCurrency: 'instructed Amount Currency',
+
+    FIToFIIntermediaryFIInfo: 'FI To FI Intermediary FI Info',
+    FIToFIBeneficiaryFIInfo: 'FI To FI Beneficiary FI Info',
+    FIToFIBeneficiaryInfo: 'FI To FI Beneficiary Info',
+    FIToFIReceiverFIInfo: 'FI To FI Receiver FI Info',
+    FIToFIIntermediaryFIAdvice: 'FI To FI Intermediary FI Advice',
+    FIToFIIntermediaryFIAdviceOther: 'FI To FI Intermediary FI Advice Other',
+    FIToFIBeneficiaryFIAdvice: 'FI To FI Beneficiary FI Advice',
+    FIToFIBeneficiaryFIAdviceOther: 'FI To FI Beneficiary FI Advice Other',
+    FIToFIBeneficiaryAdvice: 'FI To FI Beneficiary Advice',
+    FIToFIBeneficiaryAdviceOther: 'FI To FI Beneficiary Advice Other',
+    FIToFIMethodOfPaymentToBeneficiary: 'FI To FI Method Of Payment To Beneficiary',
+    FIToFIMethodOfPaymentToBeneficiaryAdditionalInfo: 'FI To FI Method Of Payment To Beneficiary Additional Info'
 }
 
 let hemera: Hemera;
@@ -65,7 +98,7 @@ describe('Test for reading entity', function () {
             cmd: 'read',
             data: { id: entity.id }
         }, (err: any, resp: any) => {
-            expect(resp.name).to.be.equals('Vladimir');
+            expect(resp.fedwirePaymentId).to.be.equals('123456789');
             expect(resp.id).to.exist;
             done();
         })
