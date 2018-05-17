8cb0a6053488bbaf153aa6ed809db1a6a28f4d0f
Jovan Damjanovic <jovandamjanovic@gmail.com>
2018-05-17 15:53:16 +0200

    model definition

--- src/model/payment-amerbank.model.ts
+++ src/model/payment-amerbank.model.ts
@@ -1,8 +1,44 @@
-import { StringSchema } from 'nats-hemera';
+import { StringSchema, NumberSchema } from 'nats-hemera';
 
 export interface IPaymentAmerbankModel {
     id?: StringSchema;
     name: StringSchema;
+
+    fedwirePaymentId: StringSchema
+
+    originatorAddressLine1: StringSchema;
+    originatorAddressLine2: StringSchema;
+    originatorAddressLine3: StringSchema;
+
+    instructingFIIdCode: StringSchema;
+    instructingFIIdentifier: StringSchema;
+    instructingFIName: StringSchema;
+    instructingFIAddressLine1: StringSchema;
+    instructingFIAddressLine2: StringSchema;
+    instructingFIAddressLine3: StringSchema;
+
+    detailsOfCharges: StringSchema;
+    detailsOfChargesCurrency: StringSchema;
+    sendersCharge1: NumberSchema;
+    sendersCharge1Currency: StringSchema;
+    sendersCharge2: NumberSchema;
+    sendersCharge2Currency: StringSchema;
+    instructedAmount: NumberSchema;
+    instructedAmountCurrency: StringSchema;
+
+    FIToFIIntermediaryFIInfo: StringSchema;
+    FIToFIBeneficiaryFIInfo: StringSchema;
+    FIToFIBeneficiaryInfo: StringSchema;
+    FIToFIReceiverFIInfo: StringSchema;
+    FIToFIIntermediaryFIAdvice: StringSchema;
+    FIToFIIntermediaryFIAdviceOther: StringSchema;
+    FIToFIBeneficiaryFIAdvice: StringSchema;
+    FIToFIBeneficiaryFIAdviceOther: StringSchema;
+    FIToFIBeneficiaryAdvice: StringSchema;
+    FIToFIBeneficiaryAdviceOther: StringSchema;
+    FIToFIMethodOfPaymentToBeneficiary: StringSchema;
+    FIToFIMethodOfPaymentToBeneficiaryAdditionalInfo: StringSchema;
+
 }
 
 export interface ID {
