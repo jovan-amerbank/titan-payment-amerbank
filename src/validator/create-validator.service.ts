a8d8b50d62055e27a244d31cdb41f566962dde87
Jovan Damjanovic <jovandamjanovic@gmail.com>
2018-05-17 16:08:25 +0200

    added validators

--- src/validator/create-validator.service.ts
+++ src/validator/create-validator.service.ts
@@ -12,7 +12,47 @@ export class CreateValidatorService {
         return this.hemera.joi.object().required()
             .keys<IPaymentAmerbankModel>(
             {
-                name: this.hemera.joi.string().required(),
+                fedwirePaymentId: this.hemera.joi.string().required(),
+
+                originatorAddressLine1: this.hemera.joi.string().required(),
+                originatorAddressLine2: this.hemera.joi.string().required(),
+                originatorAddressLine3: this.hemera.joi.string().required(),
+
+                instructingFIIdCode: this.hemera.joi.string().valid([
+                    "B - SWIFT Bank Identifier (BIC)",
+                    "C - CHIPS Participant",
+                    "D - Demand Deposit Account (DDA) Number",
+                    "F - Fed Routing Number",
+                    "T - SWIFT (BIC) or BEI and Account Number",
+                    "U - CHIPS Identifier"
+                    ]).required(),
+                instructingFIIdentifier: this.hemera.joi.string().required(),
+                instructingFIName: this.hemera.joi.string().required(),
+                instructingFIAddressLine1: this.hemera.joi.string().required(),
+                instructingFIAddressLine2: this.hemera.joi.string().required(),
+                instructingFIAddressLine3: this.hemera.joi.string().required(),
+
+                detailsOfCharges: this.hemera.joi.string().optional(),
+                detailsOfChargesCurrency: this.hemera.joi.string().optional(),
+                sendersCharge1: this.hemera.joi.number().optional(),
+                sendersCharge1Currency: this.hemera.joi.string().optional(),
+                sendersCharge2: this.hemera.joi.number().optional(),
+                sendersCharge2Currency: this.hemera.joi.string().optional(),
+                instructedAmount: this.hemera.joi.number().optional(),
+                instructedAmountCurrency: this.hemera.joi.string().optional(),
+
+                FIToFIIntermediaryFIInfo: this.hemera.joi.string().optional(),
+                FIToFIBeneficiaryFIInfo: this.hemera.joi.string().optional(),
+                FIToFIBeneficiaryInfo: this.hemera.joi.string().optional(),
+                FIToFIReceiverFIInfo: this.hemera.joi.string().optional(),
+                FIToFIIntermediaryFIAdvice: this.hemera.joi.string().optional(),
+                FIToFIIntermediaryFIAdviceOther: this.hemera.joi.string().optional(),
+                FIToFIBeneficiaryFIAdvice: this.hemera.joi.string().optional(),
+                FIToFIBeneficiaryFIAdviceOther: this.hemera.joi.string().optional(),
+                FIToFIBeneficiaryAdvice: this.hemera.joi.string().optional(),
+                FIToFIBeneficiaryAdviceOther: this.hemera.joi.string().optional(),
+                FIToFIMethodOfPaymentToBeneficiary: this.hemera.joi.string().optional(),
+                FIToFIMethodOfPaymentToBeneficiaryAdditionalInfo: this.hemera.joi.string().optional()
             });
     }
 }
