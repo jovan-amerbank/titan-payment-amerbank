6d07bd2e6f12e7cec07b065ec0e25264c7653d84
Jovan Damjanovic <jovandamjanovic@gmail.com>
2018-05-17 21:05:07 +0200

    remove pagination and extra files

--- src/action/action-factory.service.ts
+++ src/action/action-factory.service.ts
@@ -1,7 +1,6 @@
 import { Service } from 'justinject';
 import { CreateActionService } from './create-action.service';
 import { UpdateActionService } from './update-action.service';
-import { PaginationActionService } from './pagination-action.service';
 import { ReadActionService } from './read-action.service';
 import { DeleteActionService } from './delete-action.service';
 
@@ -10,7 +9,6 @@ export class ActionFactoryService {
     constructor(
         public create: CreateActionService,
         public update: UpdateActionService,
-        public pagination: PaginationActionService,
         public read: ReadActionService,
         public deleteAct: DeleteActionService,
     ) { }
@@ -18,7 +16,6 @@ export class ActionFactoryService {
     public registerActions() {
         this.create.createAction();
         this.update.updateAction();
-        this.pagination.paginationAction();
         this.read.readAction();
         this.deleteAct.deleteAction();
     }
--- src/action/create-action.service.ts
+++ src/action/create-action.service.ts
@@ -50,7 +50,7 @@ export class CreateActionService {
             });
 
             return done(null, merged);
-        });
+        })
         // })
 
 
